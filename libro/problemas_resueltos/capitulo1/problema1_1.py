6fffc889ad35a9d4c142f62aa24d85456829f154
jeshc <hcjesus@gmail.com>
2019-09-19 17:29:26 -0500

    La tarea

---
+++ libro/problemas_resueltos/capitulo1/problema1_1.py
@@ -0,0 +1,5 @@
+PREPRO=float(input("Precio del producto:?"))
+PAGO= float(input("De cuanto es tu pago:"))
+DEVO=PAGO-PREPRO
+print(f"Pagaste ${PAGO} y el precio del prod es: ${PREPRO } y tu cambio es ${DEVO} ")
+
---
+++ libro/problemas_resueltos/capitulo1/problema1_2.py
@@ -0,0 +1,5 @@
+BASE = float(input("Dame la base del triangulo:"))
+ALTU = float(input("Dame la altura del triangulo:"))
+SUP = (BASE * ALTU)/2
+print(f"El area de este triangulo es { SUP } dado que la altura es {ALTU} U. y la base es de {BASE} U.   ")
+
