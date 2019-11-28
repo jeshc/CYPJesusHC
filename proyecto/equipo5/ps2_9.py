c9f9168141991526b98106b76b0b6f2e04a116d1
CristobalMH <rawrr.rasmus@gmail.com>
2019-11-28 16:22:19 -0600

    Igualdad_Expresiones

---
+++ proyecto/equipo5/ps2_9.py
@@ -0,0 +1,9 @@
+A = int(input("Ingrese el valor de A: "))
+N = int(input("Ingrese el valor de N: "))
+
+if (A**-N) == (1/A**N):
+    print("Las ecuaciones son iguales n_n")
+else:
+    print("Las ecuaciones no coinciden u_u")
+print(A**-N)
+print(1/A**N)
